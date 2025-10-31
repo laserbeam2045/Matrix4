b43f29095235de30e4ce74e3510e64415d7a2ab7
Matrix User <user@example.com>
2025-10-31 14:31:33 +0900

    fix: root

--- plugins/server.server.ts
+++ plugins/server.server.ts
@@ -3,7 +3,8 @@ import { defineNuxtPlugin } from '#app'
 export default defineNuxtPlugin(() => {
   useState('TREE_ROOT_ID', () => '9FGOQhg2IodypRvq')
   // useState('HOME_ROOT_ID', () => 'ozmkiRCEBnh7ZT83')
-  useState('HOME_ROOT_ID', () => 'SiEBuCsBGkm/UUUe')
+  // useState('HOME_ROOT_ID', () => 'SiEBuCsBGkm/UUUe')
+  useState('HOME_ROOT_ID', () => 'ozmkiRCEBnh7ZT83')
 
   console.log('%cserver', 'color: red;')
 })
